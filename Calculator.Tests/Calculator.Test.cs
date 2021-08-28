309829573c35508948e605f7f389c507dbbc5744
Valentin P <38946427+zedow@users.noreply.github.com>
2021-08-28 13:37:24 +0200

    Calculator

---
+++ Calculator.Tests/Calculator.Test.cs
@@ -0,0 +1,72 @@
+using System;
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    public class CalculatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Addition_PositiveIntegers_ReturnCorrectResult()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Addition(1, 1);
+
+            Assert.AreEqual(2,result);
+        }
+
+        [Test]
+        public void Addition_NegativeIntegers_ReturnCorrectResult()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Addition(-1, -1);
+
+            Assert.AreEqual(-2, result);
+        }
+
+        [Test]
+        public void Multiplication_PositiveIntegers_ReturnCorrectResult()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Multiplication(3, 3);
+
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        public void Modulo_Integers_ReturnCorrectResult()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Modulo(13, 3);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void Division_Integers_ReturnCorrectResult()
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Division(9, 3);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Division_ByZero_ThrowDividedByZeroException()
+        {
+            var calculator = new Calculator();
+
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(9, 0));
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Modulo(int a, int b)
+        {
+            return a % b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
